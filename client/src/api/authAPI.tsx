e1377c6c28a23d02e2272a1d0286e22a46161f18
Benjamin Machock <43613715+benjaminmachock@users.noreply.github.com>
2024-11-04 14:41:06 -0700

    test

---
+++ client/src/api/authAPI.tsx
@@ -0,0 +1,27 @@
+import { CustomerLogin } from "../interfaces/CustomerLogin";
+
+const login = async (custInfo: CustomerLogin) => {
+  console.log(custInfo);
+  try {
+    const response = await fetch("/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(custInfo),
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(`Error: ${errorData.message}`);
+    }
+
+    const data = await response.json();
+
+    return data;
+  } catch (err) {
+    console.log("Error from customer login:", err);
+    return Promise.reject("Could not fetch customer info");
+  }
+};
+export { login };
